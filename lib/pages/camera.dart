db11f9ea3d15d6f2495749ea0e586bcb23ae774a
Justin Thoreson <thoresonjd@gmail.com>
2021-10-26 18:21:04 -0700

    Reverted back to drawer with appbar

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
-class CameraPage extends StatefulWidget {
+class CameraPage extends StatelessWidget {
   const CameraPage({ Key? key }) : super(key: key);
-
-  @override
-  _CameraPageState createState() => _CameraPageState();
-}
-
-class _CameraPageState extends State<CameraPage> {
-  final GlobalKey<ScaffoldState> _scaffoldDrawerKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        key: _scaffoldDrawerKey,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
+        ),
         drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.zero,
@@ -38,23 +37,77 @@ class _CameraPageState extends State<CameraPage> {
             ],
           ),
         ),
-        body: Stack(
-          children: [
-            IconButton(
-              onPressed: () => _scaffoldDrawerKey.currentState!.openDrawer(), 
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.black)
-            ),
-            const Center(
-              child: Icon(
-                Icons.camera_alt,
-                size: 100,
-              ),
-            ),
-          ],
+        body: const Center(
+          child: Icon(
+            Icons.camera_alt,
+            size: 100,
+          ),
         ),
       ),
     );
   }
 }
+
+// Drawer no appbar
+// class CameraPage extends StatefulWidget {
+//   const CameraPage({ Key? key }) : super(key: key);
+
+//   @override
+//   _CameraPageState createState() => _CameraPageState();
+// }
+
+// class _CameraPageState extends State<CameraPage> {
+//   final GlobalKey<ScaffoldState> _scaffoldDrawerKey = GlobalKey<ScaffoldState>(); // Drawer no appbar
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         key: _scaffoldDrawerKey, // drawer no appbar
+//         // appBar: AppBar(
+//         //   backgroundColor: Colors.transparent,
+//         //   elevation: 0,
+//         // ),
+//         drawer: Drawer(
+//           child: ListView(
+//             padding: EdgeInsets.zero,
+//             children: const <Widget>[
+//               // SizedBox(
+//               //   height: 50,
+//               //   child: DrawerHeader(
+//               //     child: Text("Color scales"),
+//               //     decoration: BoxDecoration(
+//               //       color: Colors.red,
+//               //     ),
+//               //   ),
+//               // ),
+//               ListTile(
+//                 title: Text("Scale 1"),
+//               ),
+//               ListTile(
+//                 title: Text("Scale 2"),
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: Stack(
+//           children: [
+//             IconButton(
+//               onPressed: () => _scaffoldDrawerKey.currentState!.openDrawer(), 
+//               icon: const Icon(
+//                 Icons.menu,
+//                 color: Colors.black)
+//             ),
+//             const Center(
+//               child: Icon(
+//                 Icons.camera_alt,
+//                 size: 100,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
