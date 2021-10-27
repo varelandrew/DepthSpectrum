1516cf162f4b2e4ddf50a982c872ba4c041906b1
unknown <devon.l.mckee@gmail.com>
2021-10-26 20:58:44 -0700

    Adding WebView and base64 encoded page to display page

--- lib/pages/display.dart
+++ lib/pages/display.dart
@@ -1,16 +1,44 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class DisplayPage extends StatelessWidget {
-  const DisplayPage({ Key? key }) : super(key: key);
+const String ModelViewer = '''
+<!DOCTYPE html><html>
+  <head>
+  </head>
+  <body>
+    <h1>holy shit it loaded</h1>
+    <img src="https://i.kym-cdn.com/entries/icons/mobile/000/035/807/cover1.jpg"/>
+  </body>
+</html>
+''';
+
+class DisplayPage extends StatefulWidget {
+  @override
+  _DisplayPageState createState() => _DisplayPageState();
+}
+
+class _DisplayPageState extends State<DisplayPage> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Icon(
-          Icons.photo,
-          size: 100),
-      ),
-    );
+    return Scaffold(body: Builder(builder: (BuildContext context) {
+      return WebView(onWebViewCreated: (WebViewController webViewController) {
+        final String ModelViewerBase64 =
+            base64Encode(const Utf8Encoder().convert(ModelViewer));
+        webViewController.loadUrl('data:text/html;base64,$ModelViewerBase64');
+        _controller.complete(webViewController);
+      });
+    }));
   }
 }
--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:depth_spectrum/pages/display.dart';
 
 class HistoryPage extends StatelessWidget {
-  const HistoryPage({ Key? key }) : super(key: key);
+  const HistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +20,8 @@ class HistoryPage extends StatelessWidget {
             ListTile(
               leading: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image.network("https://i2.kym-cdn.com/photos/images/original/001/023/910/f0c.jpg"),
+                child: Image.network(
+                    "https://i2.kym-cdn.com/photos/images/original/001/023/910/f0c.jpg"),
               ),
               title: const Text("Photo 1"),
             ),
@@ -27,18 +29,23 @@ class HistoryPage extends StatelessWidget {
             ListTile(
               leading: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image.network("https://media-exp1.licdn.com/dms/image/C5603AQEdAojqU6D-Cw/profile-displayphoto-shrink_400_400/0/1631410513596?e=1640822400&v=beta&t=36AkiwTverFPRiBJosm1VRC22bl5BXrUh2GzPggJw6s"),
+                child: Image.network(
+                    "https://media-exp1.licdn.com/dms/image/C5603AQEdAojqU6D-Cw/profile-displayphoto-shrink_400_400/0/1631410513596?e=1640822400&v=beta&t=36AkiwTverFPRiBJosm1VRC22bl5BXrUh2GzPggJw6s"),
               ),
               title: const Text("Photo 2"),
             ),
             const Padding(padding: EdgeInsets.all(5)),
             ListTile(
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network("https://i.kym-cdn.com/entries/icons/mobile/000/035/807/cover1.jpg"),
-              ),
-              title: const Text("Photo 3"),
-            ),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                      "https://i.kym-cdn.com/entries/icons/mobile/000/035/807/cover1.jpg"),
+                ),
+                title: const Text("Photo 3"),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => DisplayPage()));
+                }),
           ],
         ),
       ),
