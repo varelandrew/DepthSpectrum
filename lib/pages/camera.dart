27c89da5016731dac2b24bb39266a66337afa3a2
Justin Thoreson <thoresonjd@gmail.com>
2021-11-07 14:52:39 -0800

    Camera fills screen, drawer transparent

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -50,45 +50,58 @@ class _CameraState extends State<Camera> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: DSColors.black,
-        appBar: AppBar(backgroundColor: Colors.transparent),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const <Widget>[
-              SizedBox(
-                height: 70,
-                child: DrawerHeader(
-                  child: Text("Color scales"),
-                  decoration: BoxDecoration(color: DSColors.darkred),
-                ),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0),
+        drawer: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.transparent
+          ),
+          child: Drawer(
+            child: Container(
+              color: Colors.transparent,
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: const <Widget>[
+                  SizedBox(
+                    height: 70,
+                    child: DrawerHeader(
+                      child: Text("Color scales"),
+                      decoration: BoxDecoration(color: DSColors.darkred),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Image(
+                        image: NetworkImage(
+                            "https://media.discordapp.net/attachments/676149764764598300/902796804062580746/alexbanner.png")),
+                    title: Text("Protanopia", style: TextStyle(color: DSColors.white)),
+                  ),
+                  ListTile(
+                    leading: Image(
+                        image: NetworkImage(
+                            "https://cdn.discordapp.com/attachments/676149764764598300/902795432772648990/babylaugh.png")),
+                    title: Text("Deuteranopia", style: TextStyle(color: DSColors.white))
+                  ),
+                ],
               ),
-              ListTile(
-                leading: Image(
-                    image: NetworkImage(
-                        "https://media.discordapp.net/attachments/676149764764598300/902796804062580746/alexbanner.png")),
-                title: Text("Scale 1"),
-                subtitle: Text("Protanopia"),
-              ),
-              ListTile(
-                leading: Image(
-                    image: NetworkImage(
-                        "https://cdn.discordapp.com/attachments/676149764764598300/902795432772648990/babylaugh.png")),
-                title: Text("Scale 2"),
-                subtitle: Text("Deuteranopia"),
-              ),
-            ],
+            ),
           ),
         ),
         // Display camera preview if initialized, else display loading indicator
-        body: FutureBuilder<void>(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return CameraPreview(_controller);
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
+        extendBodyBehindAppBar: true,
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return CameraPreview(_controller);
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
         // Take a picture with onPressed callback
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -126,11 +139,18 @@ class DisplayImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: DSColors.black,
       appBar: AppBar(
         backgroundColor: DSColors.red,
         title: const Text('Picture display')
       ),
-      body: Image.file(File(imagePath))
+      body: Image.file(
+        File(imagePath),
+        alignment: Alignment.topCenter,
+        fit: BoxFit.contain,
+        height: double.infinity,
+        width: double.infinity
+      )
     );
   }
 }
