0a3b845f8acdeb7e1b7f8cedeea1261c74a99b23
unknown <devon.l.mckee@gmail.com>
2021-11-15 10:42:50 -0800

    Solved decimal exponent problem by calling to C lib with FFI

---
+++ lib/c_pow.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
+double c_pow(double a, double b) {
+    return pow(a, b);
+}
--- lib/model_utils.dart
+++ lib/model_utils.dart
@@ -3,6 +3,9 @@ import 'dart:math';
 import 'package:image/image.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:vector_math/vector_math.dart';
+import 'dart:ffi';
+import 'dart:io' show Platform, Directory;
+import 'package:path/path.dart' as path;
 
 // Gets storage directory by platform
 Future<String> getStorageDirectory() async {
@@ -15,6 +18,13 @@ Future<String> getStorageDirectory() async {
 
 final Matrix3 toXYZ = Matrix3(0.4124564, 0.3575761, 0.1804375, 0.2126729,
     0.7151522, 0.0721750, 0.0193339, 0.1191920, 0.9503041);
+
+final DynamicLibrary cPowLib = Platform.isAndroid
+    ? DynamicLibrary.open("libc_pow.so")
+    : DynamicLibrary.process();
+final double Function(double a, double b) c_pow = cPowLib
+    .lookup<NativeFunction<Double Function(Double a, Double b)>>("c_pow")
+    .asFunction();
 
 // Should build model from image
 bool buildDepthMap(File f) {
@@ -29,13 +39,20 @@ bool buildDepthMap(File f) {
           (p & 0xff) / 255, (p >> 8 & 0xff) / 255, (p >> 16 & 0xff) / 255);
       // Convert sRGB -> sRGB'
       Vector3 sRGBp = Vector3(
-        (sRGB.x <= 0.04045) ? sRGB.x / 12.92 : 0,
-        (sRGB.y <= 0.04045) ? sRGB.y / 12.92 : 0,
-        (sRGB.z <= 0.04045) ? sRGB.z / 12.92 : 0,
-      ); // TODO: FIX CONVERSION TO SRGBP
+        (sRGB.x <= 0.04045)
+            ? sRGB.x / 12.92
+            : c_pow((sRGB.x + 0.055) / 1.055, 2.4),
+        (sRGB.y <= 0.04045)
+            ? sRGB.y / 12.92
+            : c_pow((sRGB.y + 0.055) / 1.055, 2.4),
+        (sRGB.z <= 0.04045)
+            ? sRGB.z / 12.92
+            : c_pow((sRGB.z + 0.055) / 1.055, 2.4),
+      );
       // Multiply by matrix to convert sRGB' -> XYZ
       Vector3 XYZ = Vector3(toXYZ.dotRow(0, sRGBp), toXYZ.dotRow(1, sRGBp),
           toXYZ.dotRow(2, sRGBp));
+      print(XYZ);
     }
   }
   List<String> splitPath = f.path.split("/");
